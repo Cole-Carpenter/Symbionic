d19f10de9e5d128c086ae5c2228c9d7d056792c1
Carpenter <carpec@win.rpi.edu>
2018-12-11 07:15:47 -0500

    added crouch/jump and tweaked running timings

--- Symbionic/Assets/Player/Scripts/PlayerController.cs
+++ Symbionic/Assets/Player/Scripts/PlayerController.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	//speeds
     public float rotateSpeed = 1f;
     public float walkSpeed = 1f;
     public float runSpeed = 1f;
+	public float jumpSpeed = 1f;
+
+	//components
+	private Rigidbody rb;
+
+	//states
     private bool runActive = false;
     private bool running = false;
+	private bool crouching = false;
+
+	//timers
     private float runStart = 0f;
     private float leftStart = 0f;
     private float rightStart = 0f;
@@ -17,23 +27,28 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		SerialControllerM = GameObject.Find("SerialController").GetComponent<MyMessageListener>();
+		rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//running timers
         runStart -= Time.deltaTime;
         leftStart -= Time.deltaTime;
         rightStart -= Time.deltaTime;
-        print(running);
+
 		Debug.Log(SerialControllerM.Q);
+
+		
+		//Running
         if (Input.GetKeyDown(KeyCode.Q) || SerialControllerM.Q)
         {
-            leftStart = .1f;
+            leftStart = .275f;
         }
 
         if (Input.GetKeyDown(KeyCode.W) || SerialControllerM.W)
         {
-            rightStart = .1f;
+            rightStart = .275f;
         }
 
         if (runActive && rightStart > 0 && leftStart > 0)
@@ -44,7 +59,7 @@ public class PlayerController : MonoBehaviour {
         else if (rightStart > 0 && leftStart > 0)
         {
             runActive = true;
-            runStart = 2.5f;
+            runStart = .7f;
         }
 
         else if (runStart <= 0)
@@ -58,6 +73,7 @@ public class PlayerController : MonoBehaviour {
             transform.position += transform.forward * runSpeed * Time.deltaTime;
         }
 
+		//walking and turning
         if (!running && (Input.GetKey(KeyCode.Q) ||  SerialControllerM.Q) && (Input.GetKey(KeyCode.W) || SerialControllerM.W))
         {
             transform.position += transform.forward * walkSpeed * Time.deltaTime;
@@ -73,6 +89,28 @@ public class PlayerController : MonoBehaviour {
             transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
         }
 
+		//crouching and jumping
+		if(Input.GetKey(KeyCode.A)){
+			crouching = true;
+		}
+		else{
+			crouching = false;
+		}
+
+		if(Input.GetKey(KeyCode.S) && crouching){
+			Jump();
+		}
+
         
     }
+
+	private void Jump(){
+		 float DisstanceToTheGround = GetComponent<Collider>().bounds.extents.y;
+ 
+         bool IsGrounded = Physics.Raycast(transform.position, Vector3.down, DisstanceToTheGround + 0.1f);
+
+		 if(IsGrounded){
+		 	 rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+		 }
+	}
 }
