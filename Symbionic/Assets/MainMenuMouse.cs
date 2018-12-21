9d2be58bfdc6db6a53d4b5d877220d631742d499
Matthew Carlson <mdcpanama22@gmail.com>
2018-12-21 03:29:09 -0500

    Made a new scene for the main menu, will continue to edit on my own dev branch

---
+++ Symbionic/Assets/MainMenuMouse.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuMouse : MonoBehaviour {
+    private Animator ac;
+    public Transform[] target;
+
+    private bool moving = true;
+    public float walkSpeed;
+    public float tolerance;
+    public float rotSpeed;
+
+    private int index;
+    // Use this for initialization
+    void Start () {
+        ac = GetComponent<Animator>();
+        index = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (moving)
+        {
+            ac.SetBool("walking", true);
+            // The step size is equal to speed times frame time.
+            float step = walkSpeed * Time.deltaTime;
+
+            // Move our position a step closer to the target.
+            transform.position = Vector3.MoveTowards(transform.position, target[index].position, step);
+            Vector3 _dir = (transform.position - target[index].position).normalized;
+
+            Quaternion _look = Quaternion.LookRotation(-_dir);
+            transform.rotation = Quaternion.Slerp(transform.rotation, _look, Time.deltaTime * rotSpeed);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "chain")
+        {
+            index++;
+            if(index == target.Length)
+            {
+                moving = false;
+                ac.SetBool("walking", false);
+                ac.SetBool("idle", true);
+            }
+        }
+    }
+    }
