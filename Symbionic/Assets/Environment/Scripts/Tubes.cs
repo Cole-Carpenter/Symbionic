e4f70c629bb6c418ecbd46c94dc6fed83bf86626
Pheonix <colecarpenter@gmail.com>
2019-02-26 15:04:06 -0500

    started work on tubes, a bit more complicated than I thought, totally doable though, it'll look really cool.

---
+++ Symbionic/Assets/Environment/Scripts/Tubes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tubes : Interactable {
+
+    public List<Transform> Nodes;
+    public Transform player;
+    private Transform nextNode;
+    private bool activated = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (activated)
+        {
+            player.position = Vector3.Lerp(player.position, nextNode.position, 1f);
+        }
+	}
+
+    public override string Interact()
+    {
+        return base.Interact();
+    }
+}
--- Symbionic/Assets/UI/PauseMenu.cs
+++ Symbionic/Assets/UI/PauseMenu.cs
@@ -1,23 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour {
 
+    public enum PauseStates {Codes, Quit};
+
+    private PauseStates pauseSelect = 0;
     private bool paused = false;
     public GameObject pauseMenuUI;
+    public Button codeButton;
+    public Button quitButton;
 
     // Use this for initialization
     void Start () {
-		
-	}
+        codeButton = pauseMenuUI.transform.GetChild(1).GetComponent<Button>();
+        quitButton = pauseMenuUI.transform.GetChild(2).GetComponent<Button>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
         //pause
         if (Input.GetButtonDown("Pause"))
         {
+            if (paused)
+            {
+                pauseSelect = 0;
+            }
             Pause(paused);
+        }
+
+        if(paused && Input.GetButtonDown("Vertical_key"))
+        {
+            pauseSelect = (PauseStates)(((int)pauseSelect + 1) % 2);
+        }
+        
+        if (paused && paused && pauseSelect == PauseStates.Codes)
+        {
+            codeButton.Select();
+            codeButton.OnSelect(null);
+        }
+
+        if (paused && paused && pauseSelect == PauseStates.Quit)
+        {
+            quitButton.Select();
         }
     }
 
@@ -35,5 +62,11 @@ public class PauseMenu : MonoBehaviour {
             Time.timeScale = 0f;
             paused = true;
         }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        print("quitting");
     }
 }
