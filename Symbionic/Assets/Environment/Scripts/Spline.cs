9d36b2320d213d6835ff3b6403a39b4d8244a103
Pheonix <colecarpenter@gmail.com>
2019-03-14 18:42:20 -0400

    finally does everything I want it to, moving on to environment gussying

--- Symbionic/Assets/Environment/Scripts/Spline.cs
+++ Symbionic/Assets/Environment/Scripts/Spline.cs
@@ -6,84 +6,45 @@ public class Spline : MonoBehaviour {
 
     public Transform[] controlPoints;
 
-    public bool isLooping = true;
+    private Color[] colorsArray = { Color.white, Color.red, Color.blue, Color.magenta, Color.black };
+    private float totalLength;
+    private int pos = 1;
+    private float offset = 0;
 
-    void OnDrawGizmos()
+    /* void OnDrawGizmos()
+     {
+         Gizmos.color = Color.white;
+
+         //Draw the Catmull-Rom spline between the points
+         for (int i = 0; i < controlPoints.Length; i++)
+         {
+             //Cant draw between the endpoints
+             //Neither do we need to draw from the second to the last endpoint
+             //...if we are not making a looping line
+             if ((i == 0 || i == controlPoints.Length - 2 || i == controlPoints.Length - 1))
+             {
+                 continue;
+             }
+
+             pos = i;
+
+             DivideCurveIntoSteps();
+         }
+         pos = 1;
+     }*/
+
+    private void Start()
     {
-        Gizmos.color = Color.white;
-
-        //Draw the Catmull-Rom spline between the points
-        for (int i = 0; i < controlPoints.Length; i++)
-        {
-            //Cant draw between the endpoints
-            //Neither do we need to draw from the second to the last endpoint
-            //...if we are not making a looping line
-            if ((i == 0 || i == controlPoints.Length - 2 || i == controlPoints.Length - 1) && !isLooping)
-            {
-                continue;
-            }
-
-            DisplayCatmullRomSpline(i);
-        }
+        totalLength = SimpsonsRule(0f, 1f);
     }
 
-    void DisplayCatmullRomSpline(int pos)
+    Vector3 GetCatmullRomPosition(float t)
     {
-        //The 4 points we need to form a spline between p1 and p2
-        Vector3 p0 = controlPoints[ClampListPos(pos - 1)].position;
+        Vector3 p0 = controlPoints[pos - 1].position;
         Vector3 p1 = controlPoints[pos].position;
-        Vector3 p2 = controlPoints[ClampListPos(pos + 1)].position;
-        Vector3 p3 = controlPoints[ClampListPos(pos + 2)].position;
+        Vector3 p2 = controlPoints[pos + 1].position;
+        Vector3 p3 = controlPoints[pos + 2].position;
 
-        //The start position of the line
-        Vector3 lastPos = p1;
-
-        //The spline's resolution
-        //Make sure it's is adding up to 1, so 0.3 will give a gap, but 0.2 will work
-        float resolution = 0.2f;
-
-        //How many times should we loop?
-        int loops = Mathf.FloorToInt(1f / resolution);
-
-        for (int i = 1; i <= loops; i++)
-        {
-            //Which t position are we at?
-            float t = i * resolution;
-
-            //Find the coordinate between the end points with a Catmull-Rom spline
-            Vector3 newPos = GetCatmullRomPosition(t, p0, p1, p2, p3);
-
-            //Draw this line segment
-            Gizmos.DrawLine(lastPos, newPos);
-
-            //Save this pos so we can draw the next line segment
-            lastPos = newPos;
-        }
-    }
-
-    //Clamp the list positions to allow looping
-    int ClampListPos(int pos)
-    {
-        if (pos < 0)
-        {
-            pos = controlPoints.Length - 1;
-        }
-
-        if (pos > controlPoints.Length)
-        {
-            pos = 1;
-        }
-        else if (pos > controlPoints.Length - 1)
-        {
-            pos = 0;
-        }
-
-        return pos;
-    }
-
-    //Returns a position between 4 Vector3 with Catmull-Rom spline algorithm
-    Vector3 GetCatmullRomPosition(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
-    {
         //The coefficients of the cubic polynomial (except the 0.5f * which I added later for performance)
         Vector3 a = 2f * p1;
         Vector3 b = p2 - p0;
@@ -91,13 +52,193 @@ public class Spline : MonoBehaviour {
         Vector3 d = -p0 + 3f * p1 - 3f * p2 + p3;
 
         //The cubic polynomial: a + b * t + c * t^2 + d * t^3
-        Vector3 pos = 0.5f * (a + (b * t) + (c * t * t) + (d * t * t * t));
+        Vector3 position = 0.5f * (a + (b * t) + (c * t * t) + (d * t * t * t));
 
-        return pos;
+        return position;
     }
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
+    void DivideCurveIntoSteps()
+    {
+        Vector3 p0 = controlPoints[pos - 1].position;
+        Vector3 p1 = controlPoints[pos].position;
+        Vector3 p2 = controlPoints[pos + 1].position;
+        Vector3 p3 = controlPoints[pos + 2].position;
+
+        //How many sections do we want to divide the curve into
+        int parts = 10;
+
+        //What's the length of one section?
+        float sectionLength = totalLength / (float)parts;
+
+        //Init the variables we need in the loop
+        float currentDistance = 0f + sectionLength;
+
+        //The curve's start position
+        Vector3 lastPos = p1;
+
+        //The Bezier curve's color
+        //Need a seed or the line will constantly change color
+        Random.InitState(12345);
+
+        int lastRandom = Random.Range(0, colorsArray.Length);
+
+        for (int i = 1; i <= parts; i++)
+        {
+            //Use Newton–Raphsons method to find the t value from the start of the curve 
+            //to the end of the distance we have
+            Vector3 position = ConstantSpline(currentDistance);
+
+
+            //Draw the line with a random color
+            int newRandom = Random.Range(0, colorsArray.Length);
+
+            //Get a different random number each time
+            while (newRandom == lastRandom)
+            {
+                newRandom = Random.Range(0, colorsArray.Length);
+            }
+
+            lastRandom = newRandom;
+
+            Gizmos.color = colorsArray[newRandom];
+
+            Gizmos.DrawLine(lastPos, position);
+
+
+            //Save the last position
+            lastPos = position;
+
+            //Add to the distance traveled on the line so far
+            currentDistance += sectionLength;
+        }
+    }
+    
+    private float SimpsonsRule(float t0, float t1)
+    {
+        //This is the resolution and has to be even
+        int n = 20;
+
+        //Now we need to divide the curve into sections
+        float delta = (t1-t0) / (float)n;
+
+        //The main loop to calculate the length
+
+        //Everything multiplied by 1
+        float endPoints = ArcLengthIntegrand(t0) + ArcLengthIntegrand(t1);
+
+        //Everything multiplied by 4
+        float x4 = 0f;
+        for (int i = 1; i < n; i += 2)
+        {
+            float t = t0 + delta * i;
+
+            x4 += ArcLengthIntegrand(t);
+        }
+
+        //Everything multiplied by 2
+        float x2 = 0f;
+        for (int i = 2; i < n; i += 2)
+        {
+            float t = t0 + delta * i;
+
+            x2 += ArcLengthIntegrand(t);
+        }
+
+        //The final length
+        float length = (delta / 3f) * (endPoints + 4f * x4 + 2f * x2);
+
+        return length;
+    }
+    
+    private Vector3 CatmullDerivative(float t)
+    {
+        Vector3 p0 = controlPoints[pos - 1].position;
+        Vector3 p1 = controlPoints[pos].position;
+        Vector3 p2 = controlPoints[pos + 1].position;
+        Vector3 p3 = controlPoints[pos + 2].position;
+
+        //The coefficients of the cubic polynomial (except the 0.5f * which I added later for performance)
+        Vector3 b = p2 - p0;
+        Vector3 c = 2f * p0 - 5f * p1 + 4f * p2 - p3;
+        Vector3 d = -p0 + 3f * p1 - 3f * p2 + p3;
+
+        //The cubic polynomial derivative: b + c * t + d * t^2
+        Vector3 position = 0.5f * (b + 2 * (c * t) + 3 * (d * t * t));
+
+        return position;
+    }
+
+    private float ArcLengthIntegrand(float t)
+    {
+
+        //The derivative at this point (the velocity vector)
+        Vector3 dPos = CatmullDerivative(t);
+
+        float integrand = dPos.magnitude;
+
+        return integrand;
+    }
+
+    private Vector3 ConstantSpline(float t)
+    { 
+        float d = totalLength * t;
+
+        //Need an error so we know when to stop the iteration
+        float error = 0.001f;
+
+        //We also need to avoid infinite loops
+        int iterations = 0;
+
+        while (true)
+        {
+            //Newton's method
+            float tNext = t - ((SimpsonsRule(0f, t) - d) / ArcLengthIntegrand(t));
+
+            //Have we reached the desired accuracy?
+            if (Mathf.Abs(tNext - t) < error)
+            {
+                break;
+            }
+
+            t = tNext;
+
+            iterations += 1;
+
+            if (iterations > 1000)
+            {
+                break;
+            }
+        }
+
+        return GetCatmullRomPosition(t);
+    }
+
+    public Vector3 GetStart()
+    {
+        return controlPoints[pos].position;
+    }
+
+    public int GetNumPoints()
+    {
+        return controlPoints.Length;
+    }
+
+    public Vector3 GetNextPoint()
+    {
+        return controlPoints[pos + 1].position;
+    }
+
+    public Vector3 SplineMove(float t, Transform moving_body)
+    {
+        if ((moving_body.position - controlPoints[pos + 1].position).magnitude < 0.8f)
+        {
+            pos++;
+            offset = t;
+        }
+        if (pos == controlPoints.Length - 2)
+        {
+            return moving_body.position;
+        }
+        return ConstantSpline(t - offset);
+    }
 }
--- Symbionic/Assets/Environment/Scripts/Tubes.cs
+++ Symbionic/Assets/Environment/Scripts/Tubes.cs
@@ -6,22 +6,78 @@ public class Tubes : Interactable {
 
     public List<Transform> Nodes;
     public Transform playerT;
-    private Transform nextNode;
+    public Transform camPoint;
+    public GameObject camRig;
+    public Spline spline;
+
+    public float speed = 1f;
+
+    private UnityStandardAssets.Cameras.FreeLookCam camScript;
+    private PlayerController playerScript;
+    private Camera cam;
+
+    private bool awake = false;
+    private float realSpeed;
+    private int nPoints;
+    private float t = 0;
+    private float distance = 0;
     private bool activated = false;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Use this for initialization
+    void Start () {
+        spline = GetComponentInChildren<Spline>();
+        camScript = camRig.GetComponent<UnityStandardAssets.Cameras.FreeLookCam>();
+        cam = Camera.main;
+        playerScript = playerT.GetComponent<PlayerController>();
+        realSpeed = speed / 10;
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Interact(collision.gameObject.transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if(activated && !awake)
+        {
+            camScript.SetTarget(null);
+            camScript.enabled = false;
+            playerScript.enabled = false;
+            awake = true;
+        }
         if (activated)
         {
-            playerT.position = Vector3.Lerp(playerT.position, nextNode.position, 1f);
+            cam.transform.position = Vector3.Lerp(cam.transform.position, camPoint.position, 2f * Time.deltaTime);
+            Vector3 relativePos = playerT.position - cam.transform.position;
+            cam.transform.rotation = Quaternion.Lerp(cam.transform.rotation, Quaternion.LookRotation(relativePos, Vector3.up), 3f * Time.deltaTime);
+            Vector3 newPos = spline.SplineMove(t, playerT);
+            if(newPos == playerT.position)
+            {
+                print("here");
+                activated = false;
+            }
+            else
+            {
+                playerT.position = newPos;
+                playerT.LookAt(spline.GetNextPoint());
+                t += realSpeed;
+            }
+        }
+        if (!activated && awake)
+        {
+            cam.transform.localPosition = new Vector3(0, 0, -15);
+            cam.transform.localRotation = Quaternion.identity;
+            camScript.enabled = true;
+            camScript.SetTarget(playerT);
+            playerScript.enabled = true;
+            awake = false;
         }
 
-        print("");
 	}
 
     public override string Interact(Transform player)
